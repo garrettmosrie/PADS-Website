92543f44e5c85759ed4595c7e3f968f75cd91166
garrettmosrie <gdmosrie@gmail.com>
2025-06-23 23:09:35 -0400

    frontend updates

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import Signal from "./models/Signal.js";
+import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is ready howu");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
 });
 
 app.post("/signals", async (req, res) => {
@@ -23,12 +35,22 @@ app.post("/signals", async (req, res) => {
     });
 
     const saved = await signal.save();
+    io.emit("new-signal", saved);
     res.status(201).json(saved);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
 
-app.listen(5000, () => {
+app.get("/signals", async (req, res) => {
+  try {
+    const signals = await Signal.find().sort({ detectedAt: -1 }); // newest first
+    res.json(signals);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+server.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
 });
